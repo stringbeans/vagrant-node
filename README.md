f951960dbc77a788f8b2b8387800c5a8b0badfbc
MikaAK <mikakalathil@gmail.com>
2015-02-19 11:43:41 -0800

    Initalize repo

---
+++ README.md
@@ -0,0 +1,6 @@
+Vagrant Node
+===
+
+Vagrant node will allow you to quickly get started with edvisors development app.
+
+To start run `vagrant up && vagrant ssh`
---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+apt-get update && time apt-get dist-upgrade
+apt-get install node git -y 
+
+#&& npm install -g strongloop gulp bower
+
+#git clone git@github.com:stringbeans/jsedvisor.git
